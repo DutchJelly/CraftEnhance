307a8109848f7ee3a9a98f1f7b27d8872861eebb
DutchJelly <jelle.keulemans@gmail.com>
2020-05-24 23:33:26 +0200

    Start of shapeless matching.

--- src/main/java/com/dutchjelly/craftenhance/crafthandling/RecipeMatcher.java
+++ src/main/java/com/dutchjelly/craftenhance/crafthandling/RecipeMatcher.java
@@ -2,26 +2,30 @@ package com.dutchjelly.craftenhance.crafthandling;
 
 
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.ShapelessRecipe;
 
 public class RecipeMatcher {
 
-    private static boolean matchShapeless(ItemStack[] a, ItemStack[] b, Matcher m){
-        return false;
+
+
+    public static boolean matchShapeless(ItemStack[] a, ItemStack[] b, Matcher m){
+        if(a.length != b.length) return false;
+        //TODO account for null values....
+        boolean[] matched = new boolean[a.length];
+        for(ItemStack item : a){
+            for(int i = 0; i < b.length; i++){
+                if(matched[i]) continue;
+
+                if(m.match(item, b[i])) {
+                    matched[i] = true;
+                    break;
+                }
+            }
+            return false;
+        }
+        return true;
     }
 
-    private static boolean matchShaped(ItemStack[] a, ItemStack[] b, Matcher m){
-        return false;
-    }
-
-    public static boolean match(CustomWBRecipe wbRecipe, ShapedRecipe r, Matcher m){
-        if(r == null || wbRecipe == null) return false;
-        
-        return false;
-    }
-
-    public static boolean match(CustomWBRecipe wbRecipe, ShapelessRecipe r, Matcher m){
+    public static boolean matchShaped(ItemStack[] a, ItemStack[] b, Matcher m){
         return false;
     }
 }
